cc37f1eadb76e1a2233619f023811bcee4b2318c
William PC <williampc@slack-it.com>
2021-04-19 12:52:52 -0700

    Update copyright

--- gtk/gtk2/hello_world/hello_world.c
+++ gtk/gtk2/hello_world/hello_world.c
@@ -1,4 +1,4 @@
-
+/*
 # Copyright 2020 William PC (Slack-it)- Seattle, USA
 # All rights reserved.
 #
@@ -18,7 +18,7 @@
 #  WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
 #  OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
 #  ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-
+*/
 // Include gtk
 #include <gtk/gtk.h>
 
